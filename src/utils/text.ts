912e9e49d91d7ea008bcf0b1d98d16d3063ea2e3
koimoee <itzmerinz.dc@gmail.com>
2025-01-06 23:08:21 +0700

    chore(src/utils/text.ts): optimize `trimNewLines` function to check for non-empty delimiter before applying it

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -8,8 +8,8 @@ export function trimNewLines(str: string, delimeter?: string) {
 
   let formattedStrings = stringParts.map((part) => part.trimStart());
 
-  if (delimeter) {
-    formattedStrings = formattedStrings.map((str) => `${delimeter} ${str}`);
+  if (delimeter && delimeter.trim() !== "") {
+    formattedStrings = formattedStrings.map((str) => `${delimeter}${str}`);
   }
 
   return formattedStrings.join("\n");
