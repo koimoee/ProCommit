9d36df4cea994f036b30b3d4b2447a0d5d944c32
koimoee <itzmerinz.dc@gmail.com>
2025-01-11 04:53:34 +0700

    feat(commands): add commands for setting generator, language, message approval method, model version, custom endpoint, temperature, and max tokens

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 
-import { generateAiCommitCommand, setOpenaiApiKey } from "@commands";
+import { generateAiCommitCommand, setOpenaiApiKey, setGenerator, setLanguage, setMessageApproveMethod, setModelVersion, setCustomEndpoint, setTemperature, setMaxTokens } from "@commands";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
@@ -13,6 +13,48 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.commands.registerCommand(
       "procommit.setOpenAIApiKey",
       setOpenaiApiKey
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "procommit.setGenerator",
+      setGenerator
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "procommit.setLanguage",
+      setLanguage
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "procommit.setMessageApproveMethod",
+      setMessageApproveMethod
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "procommit.setModelVersion",
+      setModelVersion
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "procommit.setCustomEndpoint",
+      setCustomEndpoint
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "procommit.setTemperature",
+      setTemperature
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "procommit.setMaxTokens",
+      setMaxTokens
     )
   );
 }
