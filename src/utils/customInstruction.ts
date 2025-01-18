d146aa16ff6844f4a86c5fa08b6c308595e6ce7d
koimoee <itzmerinz.dc@gmail.com>
2025-01-18 19:08:23 +0700

    refactor(customInstruction.ts): simplify instruction retrieval by removing unused language instructions and emoji condition

--- src/utils/customInstruction.ts
+++ src/utils/customInstruction.ts
@@ -1,13 +1,12 @@
 import { ChatCompletionRequestMessageRoleEnum } from "openai";
 import { getLanguage, getShowEmoji } from "./configuration";
-import { englishInstructions, russianInstructions, japanInstructions, englishAssistantInstruction, japanAssistantInstruction, russianAssistantInstruction, koreanInstructions, germanInstructions, koreanAssistantInstruction, germanAssistantInstruction, emojiInstructions } from "./langInstruction";
+import { englishInstructions, russianInstructions, japanInstructions, englishAssistantInstruction, koreanInstructions, germanInstructions, emojiInstructions } from "./langInstruction";
 
 export function getSystemInstruction(): {
   role: ChatCompletionRequestMessageRoleEnum;
   content: string;
 } {
   const language = getLanguage();
-  const showEmoji = getShowEmoji();
 
   const instructionsByLanguage: Record<string, string> = {
     English: englishInstructions,
@@ -18,9 +17,6 @@ export function getSystemInstruction(): {
   };
 
   let content = instructionsByLanguage[language] || englishInstructions;
-  if (showEmoji) {
-    content += emojiInstructions;
-  }
 
   return {
     role: ChatCompletionRequestMessageRoleEnum.System,
@@ -32,20 +28,13 @@ export function getAssistantInstruction(): {
   role: ChatCompletionRequestMessageRoleEnum;
   content: string;
 } {
-  const language = getLanguage();
   const showEmoji = getShowEmoji();
 
-  const instructionsAssistantByLanguage: Record<string, string> = {
-    English: englishAssistantInstruction,
-    Japanese: japanAssistantInstruction,
-    Russian: russianAssistantInstruction,
-    Korean: koreanAssistantInstruction,
-    German: germanAssistantInstruction,
-  };
-
-  let content = instructionsAssistantByLanguage[language] || englishAssistantInstruction;
+  let content = englishAssistantInstruction;
   if (showEmoji) {
     content += emojiInstructions;
+  } else {
+    content += "";
   }
 
   return {
